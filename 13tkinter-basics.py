a260036d78980b63a807e9dc3045c8ae745f7745
Sreelakshmi S <62897491+Sreelakshmi19@users.noreply.github.com>
2021-09-25 19:03:00 +0530

    Add files via upload

---
+++ 13tkinter-basics.py
@@ -0,0 +1,12 @@
+from tkinter import *
+Screen1 = Tk()
+Screen1.geometry('750x700')
+Label1 = Label(Screen1, text = "HELLO WORLD", fg="#0939EB", 
+               bg="white", font=("Lato",50))
+Label1.pack()
+Button=Button(Screen1,text="Click me")
+Button.pack()
+Screen1.mainloop()
+
+  
+
